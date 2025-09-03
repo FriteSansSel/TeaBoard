62b21f45f55c6f2b440025752746f7232b76442c
FriteSansSel <xujohan@outlook.fr>
2025-09-04 00:54:47 +0200

    fix bug

--- frontend/src/components/CastButton.jsx
+++ frontend/src/components/CastButton.jsx
@@ -31,11 +31,11 @@ const CastButton = () => {
     }, []);
 
     return (
-        <div style={{ position: 'fixed', top: 3, right: 3 }}>
+        <div style={{ position: 'fixed', top: 10, right: 10 }}>
             <google-cast-launcher
                 style={{
-                    width: 48,
-                    height: 48,
+                    width: 18,
+                    height: 18,
                     '--connected-color': '#28a745',
                     '--disconnected-color': '#007bff',
                 }}
